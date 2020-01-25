fed15edc8ac9214e2b7f709de5faabd742590554
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-25 22:50:26 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 function App() {
+  let i = 0;
+
   const handler = () => {
-    console.log('Button click handled');
+    //  i is incremented, but the ui is not updated
+    console.log('Button click handled', i);
+    i++;
   };
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <h2>Learning React</h2>
+        <h2>Learning React {i} </h2>
         <button className='btn btn-primary' onClick={handler}>
           Click Here!
         </button>
