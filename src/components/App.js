b2b9b972ef61b2f0216ad5a877afac4d4dcb4f57
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-25 23:07:45 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,18 @@ import Navbar from './Navbar';
 import Playground from './Playground';
 
 function App() {
+  const bodyText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.`;
+
+  const header = `Welcome to Home`;
+
   return (
     <React.Fragment>
       <Navbar></Navbar>
       <div className='container'>
-        <Playground></Playground>
+        <Playground bodyText={bodyText} header={header}></Playground>
       </div>
     </React.Fragment>
   );
--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
-const Playground = () => {
+const Playground = props => {
   return (
     <React.Fragment>
       <div className='jumbotron jumbotron-fluid'>
         <div className='container'>
-          <h1 className='display-4'>Welcome to Home</h1>
-          <p className='lead'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
+          <h1 className='display-4'>{props.header}</h1>
+          <p className='lead'>{props.bodyText}</p>
         </div>
       </div>
     </React.Fragment>
