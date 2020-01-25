df3398da58bd03543c7f49683a187749cc3cda84
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-25 22:44:20 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  //  This is a react hook
+  const [counter, setCounter] = useState(1);
+
   return (
     <div className='App'>
       <header className='App-header'>
         <h2>Learning React</h2>
+        <p>Counter: {counter}</p>
       </header>
     </div>
   );
