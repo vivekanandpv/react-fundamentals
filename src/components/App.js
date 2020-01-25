90b056826b90cd49edc6dcb074ba28c422c9b734
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-25 22:47:52 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 function App() {
+  const handler = () => {
+    console.log('Button click handled');
+  };
+
   return (
     <div className='App'>
       <header className='App-header'>
         <h2>Learning React</h2>
+        <button className='btn btn-primary' onClick={handler}>
+          Click Here!
+        </button>
       </header>
     </div>
   );
