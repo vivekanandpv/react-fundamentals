9d6886937eb93d6f3ab15651c8dc341fd7047d1d
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-27 14:43:18 +0530

    done

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Button = props => {
   return (
     <React.Fragment>
-      <button className={props.css} style={{ color: props.color }}>
+      <button
+        className={props.css}
+        style={{ color: props.color }}
+        onClick={() => props.handler(props.opCode)}
+      >
         {props.name}
       </button>
     </React.Fragment>
--- src/components/Playground.js
+++ src/components/Playground.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
 const Playground = props => {
+  const [counter, setCounter] = useState(0);
+
+  const handler = opCode => {
+    if (opCode === 'inc') {
+      setCounter(counter + 1);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <React.Fragment>
       <div className='jumbotron jumbotron-fluid'>
         <div className='container'>
           <h1 className='display-4'>{props.header}</h1>
+          <h3>Counter: {counter}</h3>
           <p className='lead'>{props.bodyText}</p>
           <hr></hr>
           <Button
             color={'blue'}
             css={'btn btn-warning'}
             name={'Click Me!'}
+            handler={handler}
+            opCode={'inc'}
           ></Button>
         </div>
       </div>
